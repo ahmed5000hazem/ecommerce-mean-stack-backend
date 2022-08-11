c08c194478ce5cb941e5348b9669e2e37f4b4a0a
ahmed hazem <ahmed5000hazem@gmail.com>
2022-08-11 15:44:20 +0200

    after fixing cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors")
-const app = express(cors({
-  origin: "*",
-  methods: ["GET", "POST"],
-}));
+const app = express();
+
+app.use(cors())
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const router = require("./src/router/routes")
